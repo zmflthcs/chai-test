d122f467f9222b04fc96f6fb3489c01bf564b761
younghun <zmfltmchs@gmail.com>
2021-10-25 01:51:47 +0900

    finished

--- src/components/team-component/team-component.js
+++ src/components/team-component/team-component.js
@@ -4,51 +4,64 @@ import './team-component.css';
 class TeamComponent extends Component {
     constructor(props) {
         super(props);
-        this.team = this.props.team;
-        this.teamIndex = this.props.teamIndex;
+   
+        this.state = {
+           channelName:'',
+           channelNameValid: false,
+        };
     }
 
     componentDidMount() {
         
     }
 
-    formValidation() {
-        
+    formValidation = (e) => {
+        const valid = e.target.value.length && isNaN(e.target.value[0]);
+        this.setState({
+            channelName: e.target.value,
+            channelNameValid: valid,
+        })
     }
 
-    removeChannel(index) {
-        
+
+    addChannelClicked = () =>{
+        this.props.addChannel(this.state.channelName, this.props.teamIndex);
+        this.setState({
+            channelName: '',
+            channelNameValid: false
+        })
     }
 
-    addChannel() {
-        
+    removeChannelClicked = (teamIndex, channelIndex) => {
+        this.props.removeChannel(teamIndex,channelIndex)
     }
     
-    sort() {
-        
+    sort=()=> {
+        this.props.orderChannel(this.props.teamIndex);
     }
-
+   
     render() {
         return (
         <div>
             {
-                this.team && 
+                this.props.team && 
                 <div>
-                    <span className="team-name">{this.team.name}</span>
-                    <button className="sort">&#8597;</button>
+                    <span className="team-name">{this.props.team.name}</span>
+                    <button className="sort" onClick={this.sort}>&#8597;</button>
                     <span className="add-channel">
-                        <input placeholder="Channel name"/>
-                        <button>&#8853;</button>
+                        <input placeholder="Channel name" value={this.state.channelName} onChange={this.formValidation}/>
+                        <button onClick={this.addChannelClicked} disabled={!this.state.channelNameValid}>&#8853;</button>
                     </span>
                 </div>
             }
             {
-                this.team &&
+                this.props.team &&
                 <ul className="one">
-                    { this.team.channels && this.team.channels.map((channel, idx) => (
+        
+                    { this.props.team.channels && this.props.team.channels.map((channel, idx) => (
                         <li className="channel-name" key={channel.index}>
                             <span>{channel.name}</span>
-                            <button>&#8854;</button>
+                            <button onClick={()=>this.removeChannelClicked(this.props.teamIndex, channel.index)}>&#8854;</button>
                         </li>
                     ))}
                 </ul>
--- src/components/team-list/team-list.js
+++ src/components/team-list/team-list.js
@@ -8,6 +8,7 @@ class TeamList extends Component {
         this.teams = [];
         this.teams.push({
             name: 'Team1',
+            order: 0,
             channels: [{
               name: 'Channel1',
               index: 1
@@ -19,6 +20,7 @@ class TeamList extends Component {
         });
         this.teams.push({
             name: 'Team2',
+            order: 0,
             channels: [{
               name: 'Channel1',
               index: 1
@@ -28,39 +30,122 @@ class TeamList extends Component {
               index: 2
             }]
         });
+        this.order = {
+            default: 0,
+            asc: 1,
+            desc: 2
+        }
         this.state = {
-            teams: this.teams
+            teams: this.teams,
+            teamNameNew: '',
+            teamNameValid: false,
         };
     }
+    orderChannel = (teamIndex) => {
+        const teamOrder = this.state.teams[teamIndex].order > 1 ? 0 : this.state.teams[teamIndex].order+1;
+        let newTeam;
 
-    componentDidMount() {
-        
-    }
-
-    formValidation() {
-        
-    }
-
-    addTeam(event) {
+        if (teamOrder === this.order.default){ 
+            newTeam = this.state.teams.map((team, i)=> {
+                if(i===teamIndex){
+                    return({
+                        ...team,
+                        order: teamOrder,
+                        channels: team.channels.sort((a,b)=>(a.index-b.index))
+                    })
+                }else{
+                    return {...team}
+                }
+            
+            })
+        }else if(teamOrder === this.order.asc){
+    
+            newTeam = this.state.teams.map((team, i)=> {
+                if(i===teamIndex){
+                    return({
+                        ...team,
+                        order: teamOrder,
+                        channels: team.channels.sort((a,b)=>(a.name>b.name) ? 1: -1)
+                        })
+                }else{
+                    return {...team}
+                }});
+        }else{
+            newTeam = this.state.teams.map((team, i)=> {
+                if(i===teamIndex){
+                    return({
+                        ...team,
+                        order: teamOrder,
+                        channels: team.channels.sort((a,b)=>(b.name>a.name) ? 1: -1)
+                        })
+                }else{
+                    return {...team}
+                }});
+        }
+        this.setState({
+            teams: newTeam
+        });
     
     }
 
+    formValidation=(e)=> {
+        const valid = e.target.value.length && isNaN(e.target.value[0])
+        this.setState({
+            teamNameNew: e.target.value,
+            teamNameValid: valid,
+        })
+    }
+
+    addChannel = (name, idx) => {
+        const newTeam = this.state.teams.map((team, i)=> i===idx ? {...team, channels: team.channels.concat({name, index: team.channels.length+1})} : {...team})
+        
+        this.setState({
+            teams: newTeam
+        })
+    }
+
+    removeChannel = (teamIndex, channelIndex) => {
+        console.log(teamIndex, channelIndex)
+        const newTeam = this.state.teams.map((team, i)=> i===teamIndex ? {...team, channels: team.channels.filter(channel=> channel.index !== channelIndex) }: {...team})
+        
+        this.setState({
+            teams: newTeam
+        })
+    }
+
+    addTeam = () => {
+        const newTeam = {
+            name: this.state.teamNameNew,
+            order: 0,
+            channels: []
+        };
+        this.setState({
+            teams: this.state.teams.concat(newTeam),
+            teamNameNew:'',
+            teamNameValid: false
+        })
+    }
+
+
     render() {
+        console.log(this.state.teams);
         return (
             <div>
                 <div className="teams-list">
                     <ul>
-                        { this.teams && this.teams.map((team, idx) => (
+                            
+                        { this.state.teams && this.state.teams.map((team, idx) => {
+                           return (
                             <li key={idx}>
-                                <TeamComponent/>
+                                <TeamComponent team={team} teamIndex={idx} addChannel={this.addChannel} removeChannel={this.removeChannel} orderChannel={this.orderChannel}/>
                             </li>
-                        ))}
+                        )})}
                     </ul>  
                 </div>
                 <div className="add-team">
                     <b>Add Team</b>
-                    <input placeholder="Team name"/>
-                    <button>&#8853;</button>
+                    <input placeholder="Team name" value={this.state.teamNameNew} onChange={this.formValidation}/>
+                    <button disabled={!this.state.teamNameValid} onClick={this.addTeam}>&#8853;</button>
                 </div>
             </div>
         );
